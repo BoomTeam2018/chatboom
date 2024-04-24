7eb773e8c1aad230d6cbca9c5df1811161c56f1c
Ethan Stewart <ethanste2023@gmail.com>
2024-04-24 10:43:27 +0800

    chore: ready for deployment

--- app/src/conf/bootstrap.ts
+++ app/src/conf/bootstrap.ts
@@ -7,13 +7,13 @@ import {
 import { syncSiteInfo } from '@/admin/api/info.ts';
 import { setAxiosConfig } from '@/conf/api.ts';
 
-export const version = '1.2.4'; // version of the current build
+export const version = '1.3.0'; // version of the current build
 export const dev: boolean = getDev(); // is in development mode (for debugging, in localhost origin)
 export const deploy: boolean = true; // is production environment (for api endpoint)
 export const tokenField = getTokenField(deploy); // token field name for storing token
 
-export let apiEndpoint: string = getRestApi(deploy); // api endpoint for rest api calls
-export let websocketEndpoint: string = getWebsocketApi(deploy); // api endpoint for websocket calls
+export const apiEndpoint: string = getRestApi(deploy); // api endpoint for rest api calls
+export const websocketEndpoint: string = getWebsocketApi(deploy); // api endpoint for websocket calls
 
 setAxiosConfig({
     endpoint: apiEndpoint,
